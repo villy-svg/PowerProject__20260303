194bd4fe53a91e90a8a1902cd60017d5acf9a7ae
villy-svg <villy@powerpod.in>
2026-03-05 08:43:46 +0530

    20260305  gh-pages hosting Attempt 2

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/PowerProject__20260303/', // 👈 Replace with your exact repository name
   server: {
     open: true, // This tells Vite to open the browser automatically for you
   }
