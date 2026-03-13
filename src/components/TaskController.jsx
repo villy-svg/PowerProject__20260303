7d9bc40342b66f0902f132d4cbd98f034de19e46
villy-svg <villy@powerpod.in>
2026-03-13 21:08:09 +0530

    20260313 Charging Hubs Functions Manager 2.5

--- src/components/TaskController.jsx
+++ src/components/TaskController.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { STAGE_LIST } from '../constants/stages';
 import { createInitialTask } from '../constants/taskSchema';
 import TaskModal from './TaskModal';
