c2722de58c532b0457c8dfca9733dc29eaf7bea8
villy-svg <villy@powerpod.in>
2026-06-04 21:04:37 +0530

    260604 -- Data Manager 1.1.7

--- src/features/tutorials/TutorialHub.jsx
+++ src/features/tutorials/TutorialHub.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useMemo } from 'react';
+import React, { useState, useEffect, useMemo, useCallback } from 'react';
 import MasterPageHeader from '../../components/MasterPageHeader';
 import TutorialSlideshowViewer from './TutorialSlideshowViewer';
 import { TUTORIAL_FLOWS } from './flows';
