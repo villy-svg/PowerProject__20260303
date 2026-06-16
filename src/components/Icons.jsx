0d136522a142e9bc099a11494e0e06c4112f6991
villy-svg <villy@powerpod.in>
2026-06-16 14:30:12 +0530

    260616 RBAC in Boards 1.0.2

--- src/components/Icons.jsx
+++ src/components/Icons.jsx
@@ -286,4 +286,8 @@ export const IconInfo = (props) => (
   </IconBase>
 );
 
-
+export const IconShield = (props) => (
+  <IconBase {...props}>
+    <path d="M12 22s8-4 8-10V5l-8-3-8 3v7c0 6 8 10 8 10z" />
+  </IconBase>
+);
